9a365fa10fc3b461a236dc7f59448a71346b0efb
tasosval <tasosval@cperi.certh.gr>
2012-01-18 11:16:15 +0200

    This is the first commit for the reboot of the sharpduino library. Some very basic infrastructure. Also some unit tests.

---
+++ Sharpduino.Library.Base/FirmataEmptyBase.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using Sharpduino.Library.Base.Handlers;
+
+namespace Sharpduino.Library.Base
+{
+    /// <summary>
+    /// This is the base class for the firmata protocol. It is a bare bones implementation
+    /// that should be overriden to provide any functionality. It has no message handling.
+    /// Useful if you want to implement your own subset of the firmata protocol.
+    /// </summary>
+    public class FirmataEmptyBase : IDisposable
+    {
+        /// <summary>
+        /// The available handlers for this instance
+        /// </summary>
+        protected List<IMessageHandler> AvailableHandlers;
+
+        /// <summary>
+        /// The current handler. The next byte goes to this handler
+        /// </summary>
+        protected IMessageHandler CurrentHandler;
+
+        /// <summary>
+        /// The serial port 
+        /// </summary>
+        protected SerialPort ComPort;
+
+        protected Queue<byte> IncomingData;
+
+        protected EventManager eventManager;
+
+        public FirmataEmptyBase(string portName)
+        {
+            AvailableHandlers = new List<IMessageHandler>();
+            ComPort = new SerialPort(portName);
+        }
+
+        public virtual void Initialize()
+        {
+            throw new NotImplementedException();
+            // TODO : open port
+            // TODO : subscribe to port events
+            AddExpansionMessageHandlers();
+        }
+
+        /// <summary>
+        /// This method should be overriden to add any extension message handlers
+        /// </summary>
+        protected virtual void AddExpansionMessageHandlers(){}
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    /// <summary>
+    /// This is a firmata base class that adds all known message handlers.
+    /// It is useful for the full firmata implementation
+    /// </summary>
+    public class FirmataBase : FirmataEmptyBase
+    {
+        public FirmataBase(string portName) : base(portName){}
+
+        public override void Initialize()
+        {
+            base.Initialize();
+            AddBasicMessageHandlers();
+        }
+
+        private void AddBasicMessageHandlers()
+        {
+            AvailableHandlers.Add(new SysexFirmwareMessageHandler(eventManager));
+
+            
+        }
+    }
+}
---
+++ Sharpduino.Library.Tests/EventManagerTest.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Moq;
+using NUnit.Framework;
+using Sharpduino.Library.Base;
+using Sharpduino.Library.Base.Handlers;
+using Sharpduino.Library.Base.Messages;
+
+namespace Sharpduino.Library.Tests
+{
+    [TestFixture]
+    public class EventManagerTest
+    {
+        [Test]
+        public void Manager_Should_Call_Created_Event_In_An_Interested_Handler()
+        {
+            var mockSysexFirmwareMessageHandler = new Mock<IHandle<object>>();
+            //mockSysexFirmwareMessageHandler.Setup(p => p.Handle(It.IsAny<SysexFirmwareMessage>())).Verifiable();
+
+            var manager = new EventManager();
+            manager.Subscribe(mockSysexFirmwareMessageHandler.Object);
+
+            manager.CreateEvent(new object());
+
+            mockSysexFirmwareMessageHandler.Verify(p => p.Handle(It.IsAny<object>()),Times.Once());
+        }
+
+        [Test]
+        public void Manager_Should_Call_Created_Event_In_All_Interested_Handlers()
+        {
+            var mockSysexFirmwareMessageHandler = new Mock<IHandle<object>>();
+            var mockSysexFirmwareMessageHandler2 = new Mock<IHandle<object>>();
+
+            var manager = new EventManager();
+            manager.Subscribe(mockSysexFirmwareMessageHandler.Object);
+            manager.Subscribe(mockSysexFirmwareMessageHandler2.Object);
+
+            manager.CreateEvent(new object());
+
+            mockSysexFirmwareMessageHandler.Verify(p => p.Handle(It.IsAny<object>()), Times.Once());
+            mockSysexFirmwareMessageHandler2.Verify(p => p.Handle(It.IsAny<object>()), Times.Once());
+        }
+
+        [Test]
+        public void Manager_Should_Ignore_Not_Interested_Handlers()
+        {
+            var mockMessageHandler = new Mock<IHandle<SysexFirmwareMessage>>();
+            var mockObjectHanlder = new Mock<IHandle<object>>();
+
+            var manager = new EventManager();
+            manager.Subscribe(mockMessageHandler.Object);
+            manager.Subscribe(mockObjectHanlder.Object);
+
+            manager.CreateEvent(new object());
+
+            mockMessageHandler.Verify(p => p.Handle(It.IsAny<SysexFirmwareMessage>()),Times.Never());
+            mockObjectHanlder.Verify(p => p.Handle(It.IsAny<object>()),Times.Once());
+
+            manager.CreateEvent(new SysexFirmwareMessage());
+
+            mockMessageHandler.Verify(p => p.Handle(It.IsAny<SysexFirmwareMessage>()), Times.Once());
+            mockObjectHanlder.Verify(p => p.Handle(It.IsAny<object>()), Times.Once());
+        }
+
+        [Test]
+        public void Manager_Successfully_Unscubscribes_Handlers()
+        {
+            var mockMessageHandler = new Mock<IHandle<object>>();
+
+            var manager = new EventManager();
+            manager.Subscribe(mockMessageHandler.Object);
+            manager.UnSubscribe(mockMessageHandler.Object);
+            manager.CreateEvent(new object());
+
+            mockMessageHandler.Verify(p => p.Handle(It.IsAny<object>()),Times.Never());
+        }
+    }
+}
