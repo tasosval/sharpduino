894d0e1395db7be3e90db41c586b3c2874e71fc6
tasosval <tasosval@tasoslap7>
2012-01-25 00:46:38 +0200

    Finished with CapabilityMessageHandler

--- Sharpduino.Library.Base/Handlers/CapabilityMessageHandler.cs
+++ Sharpduino.Library.Base/Handlers/CapabilityMessageHandler.cs
@@ -2,28 +2,97 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Sharpduino.Library.Base.Constants;
+using Sharpduino.Library.Base.Exceptions;
 using Sharpduino.Library.Base.Messages;
 
 namespace Sharpduino.Library.Base.Handlers
 {
-    public class CapabilityMessageHandler : BaseMessageHandler<CapabilityMessage>
+    public class CapabilityMessageHandler : SysexMessageHandler<CapabilityMessage>
     {
+        private static readonly byte commandByte = SysexCommands.CAPABILITY_RESPONSE;
+        private enum HandlerState
+        {
+            StartEnd,
+            SysexCommand,
+            PinMode,
+            PinResolution
+        }
+
+        private HandlerState currentState;
+        private PinModes currentMode;
+        private byte currentPin;
+
         public CapabilityMessageHandler(IMessageBroker messageBroker) : base(messageBroker)
         {}
 
         protected override void OnResetHandlerState()
         {
-            throw new NotImplementedException();
+            currentState = HandlerState.StartEnd;
+            currentPin = 0;
         }
 
         public override bool CanHandle(byte firstByte)
         {
-            throw new NotImplementedException();
+            switch (currentState)
+            {
+                case HandlerState.StartEnd:
+                    return firstByte == START_MESSAGE;
+                case HandlerState.SysexCommand:
+                    return firstByte == commandByte;
+                case HandlerState.PinMode:                    
+                case HandlerState.PinResolution:
+                    return true;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
         }
 
-        public override bool Handle(byte messageByte)
+        protected override bool HandleByte(byte messageByte)
         {
-            throw new NotImplementedException();
+            switch (currentState)
+            {
+                case HandlerState.StartEnd:
+                    currentState = HandlerState.SysexCommand;
+                    return true;
+                case HandlerState.SysexCommand:
+                    currentState = HandlerState.PinMode;
+                    return true;
+                case HandlerState.PinMode:
+                    // If the message has finished Reset and return that we won't handle other bytes
+                    if (messageByte == MessageConstants.SYSEX_END)
+                    {
+                        ResetHandlerState();
+                        return false;
+                    }
+
+                    // We finished with a pin so create a capability message for this pin
+                    // we don't change the currentState because the next byte could be
+                    // a mode byte, or a finish message byte
+                    if (messageByte == MessageConstants.FINISHED_PIN_CAPABILITIES)
+                    {
+                        message.PinNo = currentPin++;
+                        messageBroker.CreateEvent(message);
+                        message = new CapabilityMessage();
+                        return true;
+                    }
+
+                    // Some assurance that we get an actual mode
+                    if (messageByte > Enum.GetValues(typeof(PinModes)).Length)
+                    {
+                        ResetHandlerState();
+                        throw new MessageHandlerException(BaseExceptionMessage + "There is no such pin mode");
+                    }
+                    currentMode = (PinModes) messageByte;
+                    currentState = HandlerState.PinResolution;
+                    return true;
+                case HandlerState.PinResolution:
+                    message.Modes[currentMode] = messageByte;
+                    currentState = HandlerState.PinMode;
+                    return true;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
         }
     }
 }
--- Sharpduino.Library.Tests/Handlers/CapabilityMessageHandlerTest.cs
+++ Sharpduino.Library.Tests/Handlers/CapabilityMessageHandlerTest.cs
@@ -3,25 +3,29 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Moq;
 using NUnit.Framework;
 using Sharpduino.Library.Base.Constants;
 using Sharpduino.Library.Base.Exceptions;
 using Sharpduino.Library.Base.Handlers;
+using Sharpduino.Library.Base.Messages;
 
 namespace Sharpduino.Library.Tests.Handlers
 {
     [TestFixture]
     public class CapabilityMessageHandlerTest : BaseMessageHandlerTest<CapabilityMessageHandler>
     {
-        private readonly byte[] sampleCapabilityBytes = new byte[] { 0x14, 0x76, 0x02 };
+        private readonly byte[] sampleCapabilityBytes = new byte[] {0x14, 0x76, 0x02};
 
         private byte[] CreateMessageBytes(params byte[] capabilityBytes)
         {
             var bytes = new List<byte>
-            {
-                MessageConstants.SYSEX_START, // Start message
-                SysexCommands.CAPABILITY_RESPONSE, // Message Type                
-            };
+                            {
+                                MessageConstants.SYSEX_START,
+                                // Start message
+                                SysexCommands.CAPABILITY_RESPONSE,
+                                // Message Type                
+                            };
 
             bytes.AddRange(capabilityBytes);
             bytes.Add(MessageConstants.SYSEX_END);
@@ -34,24 +38,74 @@ namespace Sharpduino.Library.Tests.Handlers
             return new CapabilityMessageHandler(mockBroker.Object);
         }
 
-        [Test,TestCaseSource("Cases")]
+        [Test, TestCaseSource(typeof(MyFactoryClass), "CasesSuccess")]
         public void Successful_Message(params byte[] capabilityBytes)
         {
             var bytes = CreateMessageBytes(capabilityBytes);
-            throw new NotImplementedException();
+
+            for (int i = 0; i < bytes.Length - 1; i++)
+            {
+                Assert.IsTrue(handler.CanHandle(bytes[i]));
+                Assert.IsTrue(handler.Handle(bytes[i]));
+            }
+
+            Assert.IsTrue(handler.CanHandle(bytes.Last()));
+            Assert.IsFalse(handler.Handle(bytes.Last()));
         }
 
-        [Test, TestCaseSource("Cases")]
-        public void Successful_Message_Raises_Appropriate_Event(params byte[] capabilityBytes)
+        [Test, TestCaseSource(typeof (MyFactoryClass), "Cases")]
+        public void Handler_Resets_After_Successful_Message(params byte[] capabilityBytes)
         {
             var bytes = CreateMessageBytes(capabilityBytes);
-            throw new NotImplementedException();
+
+            for (int i = 0; i < bytes.Length; i++)
+                handler.Handle(bytes[i]);
+
+            Assert.IsTrue(handler.CanHandle(bytes[0]));
         }
 
-        [Test]
-        public void Handler_Resets_After_Successful_Message()
+        [Test, TestCaseSource(typeof(MyFactoryClass), "ReturnCases")]
+        public int Successful_Message_Raises_Appropriate_Events(params byte[] capabilityBytes)
         {
-            throw new NotImplementedException();
+            var bytes = CreateMessageBytes(capabilityBytes);
+            var countMessage = 0;
+            mockBroker.Setup(p => p.CreateEvent(It.IsAny<CapabilityMessage>())).Callback(() => countMessage++);
+
+            for (int i = 0; i < bytes.Length; i++)
+                handler.Handle(bytes[i]);
+
+            return countMessage;
+        }
+
+
+        [Test]
+        public void Successful_Message_Raises_Event_With_The_Right_Values()
+        {
+            var bytes = CreateMessageBytes(new byte[]
+                                               {
+                                                   (byte) PinModes.Input, 1, (byte) PinModes.Output, 1,
+                                                   (byte) PinModes.Analog, 10,
+                                                   MessageConstants.FINISHED_PIN_CAPABILITIES, 
+                                                   (byte) PinModes.Analog, 10,
+                                                   MessageConstants.FINISHED_PIN_CAPABILITIES
+                                               });
+
+            for (int i = 0; i < bytes.Length; i++)
+                handler.Handle(bytes[i]);
+
+            mockBroker.Verify(p => p.CreateEvent(
+                It.Is<CapabilityMessage>(
+                    mes => mes.PinNo == 0 &&
+                           mes.Modes.Keys.Contains(PinModes.Input) && mes.Modes[PinModes.Input] == 1 &&
+                           mes.Modes.Keys.Contains(PinModes.Output) && mes.Modes[PinModes.Output] == 1 &&
+                           mes.Modes.Keys.Contains(PinModes.Analog) && mes.Modes[PinModes.Analog] == 10
+                    )), Times.Once());
+
+            mockBroker.Verify(p => p.CreateEvent(
+                It.Is<CapabilityMessage>(
+                    mes => mes.PinNo == 1 &&
+                           mes.Modes.Keys.Contains(PinModes.Analog) && mes.Modes[PinModes.Analog] == 10
+                    )), Times.Once());
         }
 
         [Test]
@@ -66,27 +120,73 @@ namespace Sharpduino.Library.Tests.Handlers
             base.Throws_Error_If_Forced_Other_Message();
         }
 
-        static object[] Cases =
-        {
-            new byte[] { (byte) PinModes.Input, 1, (byte) PinModes.Output, 1,  (byte) PinModes.Analog, 10, MessageConstants.FINISHED_PIN_CAPABILITIES},
-            new byte[] { (byte) PinModes.Input, 1, (byte) PinModes.Output, 1,  (byte) PinModes.Analog, 10, MessageConstants.FINISHED_PIN_CAPABILITIES, (byte) PinModes.Analog, 10, MessageConstants.FINISHED_PIN_CAPABILITIES },
-            new byte[] {  } 
-        };
+        private static object[] Cases = {
+                                            new []
+                                                {
+                                                    (byte) PinModes.Input, (byte) 1, (byte) PinModes.Output, (byte) 1,
+                                                    (byte) PinModes.Analog, (byte) 10, MessageConstants.FINISHED_PIN_CAPABILITIES
+                                                }
+                                            ,
+                                            new []
+                                                {
+                                                    (byte) PinModes.Input, (byte) 1, (byte) PinModes.Output, (byte) 1,
+                                                    (byte) PinModes.Analog, (byte) 10, MessageConstants.FINISHED_PIN_CAPABILITIES,
+                                                    (byte) PinModes.Analog, (byte) 10, MessageConstants.FINISHED_PIN_CAPABILITIES
+                                                }
+                                        };
+    }
 
-        public class MyFactoryClass
+    public class MyFactoryClass
+    {
+        public static IEnumerable ReturnCases
         {
-            public static IEnumerable TestCases
+            get
             {
-                get
-                {
-                    yield return new TestCaseData(new byte[] { (byte)PinModes.Input, 1, (byte)PinModes.Output, 1, (byte)PinModes.Analog, 10, MessageConstants.FINISHED_PIN_CAPABILITIES }).Returns(1);
-                    yield return new TestCaseData(new byte[] { (byte) PinModes.Input, 1, (byte) PinModes.Output, 1,  (byte) PinModes.Analog, 10, MessageConstants.FINISHED_PIN_CAPABILITIES, (byte) PinModes.Analog, 10, MessageConstants.FINISHED_PIN_CAPABILITIES }).Returns(2);
-                    yield return new TestCaseData(12, 4).Returns(2);
-                    yield return new TestCaseData(new byte[] { 0xFF, 0})
-                      .Throws(typeof(MessageHandlerException))
-                      .SetName("Send Wrong PinMode")
-                      .SetDescription("An exception is expected");
-                }
+                yield return new TestCaseData(new byte[] { (byte)PinModes.Input, 1, (byte)PinModes.Output, 1, (byte)PinModes.Analog, 10, MessageConstants.FINISHED_PIN_CAPABILITIES }).Returns(1).SetName("One Pin");
+                yield return new TestCaseData(new byte[] { (byte) PinModes.Input, 1, (byte) PinModes.Output, 1,  (byte) PinModes.Analog, 10, MessageConstants.FINISHED_PIN_CAPABILITIES, (byte) PinModes.Analog, 10, MessageConstants.FINISHED_PIN_CAPABILITIES }).Returns(2).SetName("Two Pins");
+                yield return new TestCaseData(new byte[] { 0xFF, 0})
+                    .Throws(typeof(MessageHandlerException))
+                    .SetName("Send Wrong PinMode")
+                    .SetDescription("An exception is expected").Returns(0);
+                yield break;
+            }
+        }
+
+        public static IEnumerable Cases
+        {
+            get
+            {
+                yield return new TestCaseData(
+                                 new[]{
+                                     (byte) PinModes.Input, (byte) 1, (byte) PinModes.Output, (byte) 1,
+                                     (byte) PinModes.Analog, (byte) 10, MessageConstants.FINISHED_PIN_CAPABILITIES
+                                 }).SetName("One Pin Simple");
+                yield return new TestCaseData(new[]
+                                 {
+                                     (byte) PinModes.Input, (byte) 1, (byte) PinModes.Output, (byte) 1,
+                                     (byte) PinModes.Analog, (byte) 10, MessageConstants.FINISHED_PIN_CAPABILITIES,
+                                     (byte) PinModes.Analog, (byte) 10, MessageConstants.FINISHED_PIN_CAPABILITIES
+                                 }).SetName("Two Pins Simple");
+                yield break;
+            }
+        }
+
+        public static IEnumerable CasesSuccess
+        {
+            get
+            {
+                yield return new TestCaseData(
+                                 new[]{
+                                     (byte) PinModes.Input, (byte) 1, (byte) PinModes.Output, (byte) 1,
+                                     (byte) PinModes.Analog, (byte) 10, MessageConstants.FINISHED_PIN_CAPABILITIES
+                                 }).SetName("One Pin Success");
+                yield return new TestCaseData(new[]
+                                 {
+                                     (byte) PinModes.Input, (byte) 1, (byte) PinModes.Output, (byte) 1,
+                                     (byte) PinModes.Analog, (byte) 10, MessageConstants.FINISHED_PIN_CAPABILITIES,
+                                     (byte) PinModes.Analog, (byte) 10, MessageConstants.FINISHED_PIN_CAPABILITIES
+                                 }).SetName("Two Pins Success");
+                yield break;
             }
         }
     }
