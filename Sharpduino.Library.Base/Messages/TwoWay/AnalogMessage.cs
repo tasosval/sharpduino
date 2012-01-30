917d4ac8569d80865f5a03b21633b7012c4df675
tasosval <tasosval@laptop>
2012-01-30 23:50:47 +0200

    Started the implementation of the message creators. Also some code into the firmata class

--- Sharpduino.Library.Base/Messages/TwoWay/AnalogMessage.cs
+++ Sharpduino.Library.Base/Messages/TwoWay/AnalogMessage.cs
@@ -1,8 +1,8 @@
-namespace Sharpduino.Library.Base.Messages.Receive
+namespace Sharpduino.Library.Base.Messages.TwoWay
 {
     public class AnalogMessage
     {
-        public int Pin { get; set; }
+        public byte Pin { get; set; }
         public int Value { get; set; }
     }
 }
