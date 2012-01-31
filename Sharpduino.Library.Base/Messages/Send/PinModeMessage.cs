70651093d32c179f516fa2f4a442512f7cbb9da4
tasosval <tasosval@cperi.certh.gr>
2012-01-31 13:26:12 +0200

    Finished the PinModeMessageCreator

---
+++ Sharpduino.Library.Base/Messages/Send/PinModeMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sharpduino.Library.Base.Constants;
+
+namespace Sharpduino.Library.Base.Messages.Send
+{
+    public class PinModeMessage
+    {
+        public byte Pin { get; set; }
+        public PinModes Mode { get; set; }
+    }
+}
--- Sharpduino.Library.Tests/Creators/DigitalMessageCreatorTest.cs
+++ Sharpduino.Library.Tests/Creators/DigitalMessageCreatorTest.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using NUnit.Framework;
+using Sharpduino.Library.Base;
+using Sharpduino.Library.Base.Constants;
 using Sharpduino.Library.Base.Creators;
 using Sharpduino.Library.Base.Exceptions;
 using Sharpduino.Library.Base.Messages.TwoWay;
@@ -10,15 +12,27 @@ using Sharpduino.Library.Base.Messages.TwoWay;
 namespace Sharpduino.Library.Tests.Creators
 {
     [TestFixture]
-    public class DigitalMessageCreatorTest
+    public class DigitalMessageCreatorTest : BaseMessageCreatorTest
     {
         [Test]
-        public void Creates_Appropriate_Message()
+        public override void Creates_Appropriate_Message()
         {
-            throw new NotImplementedException();
+            var pins = new bool[]{true,false,true,true,false,false,true,false};
+            var value = BitHelper.PinVals2PortVal(pins);
+
+            var creator = new DigitalMessageCreator();
+            var returnBytes = creator.CreateMessage(new DigitalMessage() {PinStates = pins, Port = 5});
+
+            byte lsb, msb;
+            BitHelper.IntToBytes(value,out lsb, out msb);
+
+            Assert.AreEqual(returnBytes[0],(byte)(MessageConstants.DIGITAL_MESSAGE | 0x05));
+            Assert.AreEqual(returnBytes[1],lsb);
+            Assert.AreEqual(returnBytes[2],msb);            
         }
 
-        public void Throws_Error_On_Wrong_Message()
+        [Test]
+        public override void Throws_Error_On_Wrong_Message()
         {
             var creator = new DigitalMessageCreator();
             Assert.Throws<MessageCreatorException>(() => creator.CreateMessage(new AnalogMessage()));
