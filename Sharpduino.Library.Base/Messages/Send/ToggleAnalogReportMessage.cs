af750f056e6194688036f4258c687e35d12d1e5c
tasosval <tasosval@laptop>
2012-01-31 20:12:54 +0200

    ToggleAnalog and Digital Report Message Creators with tests (not live)

---
+++ Sharpduino.Library.Base/Messages/Send/ToggleAnalogReportMessage.cs
@@ -0,0 +1,8 @@
+﻿namespace Sharpduino.Library.Base.Messages.Send
+{
+    public class ToggleAnalogReportMessage
+    {
+        public byte Pin { get; set; }
+        public bool ShouldBeEnabled { get; set; }
+    }
+}
---
+++ Sharpduino.Library.Tests/Creators/ToggleDigitalReportMessageCreatorTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Sharpduino.Library.Base.Constants;
+using Sharpduino.Library.Base.Creators;
+using Sharpduino.Library.Base.Exceptions;
+using Sharpduino.Library.Base.Messages.Send;
+using Sharpduino.Library.Base.Messages.TwoWay;
+
+namespace Sharpduino.Library.Tests.Creators
+{
+    [TestFixture]
+    public class ToggleDigitalReportMessageCreatorTest : BaseMessageCreatorTest
+    {
+        [Test]
+        public override void Creates_Appropriate_Message()
+        {
+            var creator = new ToggleDigitalReportMessageCreator();
+            var bytes = creator.CreateMessage(new ToggleDigitalReportMessage {Port = 4, ShouldBeEnabled = true});
+
+            Assert.AreEqual(bytes[0],MessageConstants.REPORT_DIGITAL_PORT | 4);
+            Assert.AreEqual(bytes[1],1);
+        }
+
+        [Test]
+        public void Creates_Appropriate_Message_Off()
+        {
+            var creator = new ToggleDigitalReportMessageCreator();
+            var bytes = creator.CreateMessage(new ToggleDigitalReportMessage { Port = 4, ShouldBeEnabled = false });
+
+            Assert.AreEqual(bytes[0], MessageConstants.REPORT_DIGITAL_PORT | 4);
+            Assert.AreEqual(bytes[1], 0);
+        }
+
+        [Test]
+        public void Throws_Error_On_Wrong_Port()
+        {
+            var creator = new ToggleDigitalReportMessageCreator();
+            Assert.Throws<MessageCreatorException>(() => creator.CreateMessage(new ToggleDigitalReportMessage {Port = 17}));
+        }
+
+        [Test]
+        public override void Throws_Error_On_Wrong_Message()
+        {
+            var creator = new ToggleDigitalReportMessageCreator();
+            Assert.Throws<MessageCreatorException>(() =>creator.CreateMessage(new AnalogMessage()));
+        }
+    }
+}
+
