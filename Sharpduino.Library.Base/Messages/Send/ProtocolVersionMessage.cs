2209617761cad34cf0f535882943f72eb27141cf
tasosval@cperi <tasosval@cperi.certh.gr>
2012-01-31 14:58:26 +0200

    Added the StaticMessageCreator for static messages (they don't have any properties)

---
+++ Sharpduino.Library.Base/Messages/Send/ProtocolVersionMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sharpduino.Library.Base.Constants;
+
+namespace Sharpduino.Library.Base.Messages.Send
+{
+    public class ProtocolVersionRequestMessage : StaticMessage
+    {
+        public ProtocolVersionRequestMessage() : 
+            base(new byte[]{MessageConstants.PROTOCOL_VERSION})
+        {}
+    }
+}
